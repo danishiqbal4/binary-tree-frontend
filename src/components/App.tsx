d3bf9f143473d8d69b6d90d59bb7994e2601e6a9
Sheikh Danish Iqbal <danishiqbal4@gmail.com>
2022-04-10 15:53:12 +0530

    Initial react app

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component<{}, { items: any, apiFetch: Boolean }> {
+
+    render() {
+        return(<h1>TEST</h1>);
+    }
+
+};
+
+export default App;
